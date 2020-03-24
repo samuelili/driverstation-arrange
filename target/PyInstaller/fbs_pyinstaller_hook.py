3811194a52b67d5115fdce465b43bbed7d0895c9
Samuel Li <samuelli4521@gmail.com>
2020-03-24 14:05:39 -0700

    initial commit 2

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'Driverstation Frame', 'author': 'Samuel Li', 'version': '0.0.0', 'environment': 'local'}
